c74b271d76ca57bb4264014957879683baa24fa4
MrPan109 <56900454+MrPan109@users.noreply.github.com>
2019-11-01 18:42:45 +0800

    Create yyzs.js

---
+++ Quantumult X-Self-use/yyzs.js
@@ -0,0 +1,46 @@
+/**
+ * @supported F7664BCE
+ */
+/*
+# 用药助手解锁专业版 (By Primovist)
+^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat\?|init\?timestamp=)) url script-response-body Self-User/Script-other/Surge/JS/yyzs.js
+#用药助手解锁专业版
+hostname = newdrugs.dxy.cn
+*/
+
+const path1 = "/snsapi/username/";
+const path2 = "/app/user/pro/stat?";
+const path3 = "/app/user/init?timestamp=";
+
+const url = $request.url;
+let body = $response.body;
+
+if (url.indexOf(path1) != -1){
+body = JSON.parse(body);
+body.items.expertUser = true;
+body.items.expert = true;
+body.items.expertStatus = 1;
+body.items.professional = true;
+body = JSON.stringify(body);
+}
+
+if (url.indexOf(path2) != -1){
+body = JSON.parse(body);
+body.data.isActive = true;
+body = JSON.stringify(body);
+}
+
+if (url.indexOf(path3) != -1){
+body = JSON.parse(body);
+body.data.isProActive = true;
+body.data.expireDate = 2048;
+body.data.memberDiscount = true;
+body.data.iapPurchaseVO.purchase = true;
+body.data.iapPurchaseVO.message = null;
+body.data.iapPurchaseVO.error = null;
+body = JSON.stringify(body);
+}
+
+$done({body})
+
+// by Primovist
