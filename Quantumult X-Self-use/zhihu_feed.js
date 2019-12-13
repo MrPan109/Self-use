939c120701bd0701e5a4df3e35e5b2f1790ffaa0
MrPan109 <56900454+MrPan109@users.noreply.github.com>
2019-12-13 20:20:52 +0800

    Update zhihu_feed.js

--- Quantumult X-Self-use/zhihu_feed.js
+++ Quantumult X-Self-use/zhihu_feed.js
@@ -1,3 +1,8 @@
+/*
+Remove Zhihu ads
+Regex: ^https://api.zhihu.com/moments\?(action|feed_type)
+*/
+
 let body = $response.body
 body=JSON.parse(body)
 body['data'].forEach((element, index)=>{
@@ -7,3 +12,5 @@ body['data'].forEach((element, index)=>{
 })
 body=JSON.stringify(body)
 $done({body})
+
+// by onewayticket255
