35bb97f356b08bcaf83a1d3756cf8fb2b2cca36c
MrPan109 <56900454+MrPan109@users.noreply.github.com>
2020-01-11 10:09:48 +0800

    Update TieBa_GetCookie_QX.js

--- TieBa-DailyBonus/TieBa_GetCookie_QX.js
+++ TieBa-DailyBonus/TieBa_GetCookie_QX.js
@@ -1,14 +1,21 @@
 /*
+
 TieBa Checkin Get Cookie.
+
 About the author:
 Telegram channel: @NobyDa
 Telegram bots: @NobyDa_bot
+
 Description :
 When TieBa app is opened, click "My", If notification gets cookie success, you can use the check in script. because script will automatically judgment whether the cookie is updated, so you dont need to disable it manually.
+
 Note that the following config is only a local script configuration, please put this scripts into Quantumult X/Script, and the cookie script only works for TieBa apps in china apple store
+
 [rewrite_local]
 https?:\/\/c\.tieba\.baidu\.com\/c\/s\/login url script-response-body TieBa_GetCookie_QX.js
+
 # MITM = c.tieba.baidu.com
+
 */
 
 var headerCookie = $request.headers["Cookie"];
