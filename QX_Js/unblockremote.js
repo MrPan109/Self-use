28d31d83949981c2e6f9e34ffafa30e684eeeb5a
MrPan109 <56900454+MrPan109@users.noreply.github.com>
2020-02-28 11:40:42 +0800

    QX 简化设备ID流程

--- QX_Js/unblockremote.js
+++ QX_Js/unblockremote.js
@@ -6,7 +6,7 @@
 QX
 简化设备ID流程
 [rewrite_local]
-^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body NobyDa/unblockremote.js
+^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body https://raw.githubusercontent.com/MrPan109/Self-use/master/QX_Js/unblockremote.js
 ~~~~~~~~~~~~~~~~
 MITM = raw.githubusercontent.com, *.github.io,
 ~~~~~~~~~~~~~~~~
