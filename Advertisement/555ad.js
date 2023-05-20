57422199a180a69af52cd2bc04409dd36a294cce
MrPan <pcq727@gmail.com>
2023-05-20 13:08:22 +0800

    555ad 555ad.js

--- Advertisement/555ad.js
+++ Advertisement/555ad.js
@@ -1,1 +1,6 @@
-let obj=JSON.parse($response.body);obj.data=obj.data.filter(t=>"advert_self"!==t.layout),obj.data.forEach(t=>{t.list=t.list.filter(t=>3!==t.type)}),$done({body:JSON.stringify(obj)});
+var obj = JSON.parse($response.body);
+obj.data = obj.data.filter(t => t.layout !== "advert_self");
+obj.data.forEach(t => {
+  t.list = t.list.filter(t => t.type !== 3);
+});
+$done({ body: JSON.stringify(obj) });
