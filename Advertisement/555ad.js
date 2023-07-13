987f5f6af47d1f858015378c6b407193989a640c
MrPan109 <56900454+MrPan109@users.noreply.github.com>
2023-07-13 21:10:48 +0800

    Update 555ad.js

--- Advertisement/555ad.js
+++ Advertisement/555ad.js
@@ -1,6 +1,90 @@
-var obj = JSON.parse($response.body);
-obj.data = obj.data.filter(t => t.layout !== "advert_self");
-obj.data.forEach(t => {
-  t.list = t.list.filter(t => t.type !== 3);
-});
-$done({ body: JSON.stringify(obj) });
+var setInterval = () => {}
+var clearInterval = () => {}
+var $task = {
+  fetch: url => {
+    return new Promise((resolve, reject) => {
+      if (url.method == 'POST') {
+        $httpClient.post(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      } else {
+        $httpClient.get(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      }
+    })
+  },
+}
+
+var $prefs = {
+  valueForKey: key => {
+    return $persistentStore.read(key)
+  },
+  setValueForKey: (val, key) => {
+    return $persistentStore.write(val, key)
+  },
+}
+
+var $notify = (title = '', subt = '', desc = '', opts) => {
+  const toEnvOpts = (rawopts) => {
+    if (!rawopts) return rawopts 
+    if (typeof rawopts === 'string') {
+      if ('undefined' !== typeof $loon) return rawopts
+      else if('undefined' !== typeof $rocket) return rawopts
+      else return { url: rawopts }
+    } else if (typeof rawopts === 'object') {
+      if ('undefined' !== typeof $loon) {
+        let openUrl = rawopts.openUrl || rawopts.url || rawopts['open-url']
+        let mediaUrl = rawopts.mediaUrl || rawopts['media-url']
+        return { openUrl, mediaUrl }
+      } else {
+        let openUrl = rawopts.url       $httpClient.postrawopts['open-url']
+        if('undefined' !== typeof $rocket) return openUrl
+        return { url: openUrl }
+      }
+    } else {
+      return undefined
+    }
+  }
+  console.log(title, subt, desc, toEnvOpts(opts))
+  $notification.post(title, subt, desc, toEnvOpts(opts))
+}
+var _scriptSonverterDone = (val = {}) => {
+  let result
+  if (
+    (typeof $request !== 'undefined' &&
+    typeof val === 'object' &&
+    typeof val.status !== 'undefined' &&
+    typeof val.headers !== 'undefined' &&
+    typeof val.body !== 'undefined') || false
+  ) {
+    result = { response: val }
+  } else {
+    result = val
+  }
+  console.log('$done')
+  try {
+    console.log(JSON.stringify(result))
+  } catch (e) {
+    console.log(result)
+  }
+  $done(result)
+}
