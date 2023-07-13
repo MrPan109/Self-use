d10eef1202deda1be1b6e661435297ef889cf32d
MrPan109 <56900454+MrPan109@users.noreply.github.com>
2023-07-13 21:31:33 +0800

    Update 555ad.js

--- Advertisement/555ad.js
+++ Advertisement/555ad.js
@@ -1,90 +1,12 @@
-var setInterval = () => {}
-var clearInterval = () => {}
-var $task = {
-  fetch: url => {
-    return new Promise((resolve, reject) => {
-      if (url.method == 'POST') {
-        $httpClient.post(url, (error, response, data) => {
-          if (response) {
-            response.body = data
-            resolve(response, {
-              error: error,
-            })
-          } else {
-            resolve(null, {
-              error: error,
-            })
-          }
-        })
-      } else {
-        $httpClient.get(url, (error, response, data) => {
-          if (response) {
-            response.body = data
-            resolve(response, {
-              error: error,
-            })
-          } else {
-            resolve(null, {
-              error: error,
-            })
-          }
-        })
-      }
-    })
-  },
-}
+let body = $response.body;
+let obj = JSON.parse(body);
 
-var $prefs = {
-  valueForKey: key => {
-    return $persistentStore.read(key)
-  },
-  setValueForKey: (val, key) => {
-    return $persistentStore.write(val, key)
-  },
-}
+// 过滤掉广告
+obj.data = obj.data.filter(item => item.layout !== "advert_self");
 
-var $notify = (title = '', subt = '', desc = '', opts) => {
-  const toEnvOpts = (rawopts) => {
-    if (!rawopts) return rawopts 
-    if (typeof rawopts === 'string') {
-      if ('undefined' !== typeof $loon) return rawopts
-      else if('undefined' !== typeof $rocket) return rawopts
-      else return { url: rawopts }
-    } else if (typeof rawopts === 'object') {
-      if ('undefined' !== typeof $loon) {
-        let openUrl = rawopts.openUrl || rawopts.url || rawopts['open-url']
-        let mediaUrl = rawopts.mediaUrl || rawopts['media-url']
-        return { openUrl, mediaUrl }
-      } else {
-        let openUrl = rawopts.url       $httpClient.postrawopts['open-url']
-        if('undefined' !== typeof $rocket) return openUrl
-        return { url: openUrl }
-      }
-    } else {
-      return undefined
-    }
-  }
-  console.log(title, subt, desc, toEnvOpts(opts))
-  $notification.post(title, subt, desc, toEnvOpts(opts))
-}
-var _scriptSonverterDone = (val = {}) => {
-  let result
-  if (
-    (typeof $request !== 'undefined' &&
-    typeof val === 'object' &&
-    typeof val.status !== 'undefined' &&
-    typeof val.headers !== 'undefined' &&
-    typeof val.body !== 'undefined') || false
-  ) {
-    result = { response: val }
-  } else {
-    result = val
-  }
-  console.log('$done')
-  try {
-    console.log(JSON.stringify(result))
-  } catch (e) {
-    console.log(result)
-  }
-  $done(result)
-}
+// 过滤掉 type 为 3 的元素
+obj.data.forEach(item => {
+    item.list = item.list.filter(subItem => subItem.type !== 3);
+});
+
+$done({body: JSON.stringify(obj)});
